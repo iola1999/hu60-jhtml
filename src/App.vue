19da5296e86b316326886b6ad7547940d3fb21d2
iola1999 <iola1999@foxmail.com>
2020-01-13 22:23:16 +0800

    不缓存帖子详情页面，因为似乎没啥作用

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <transition :name="transitionName">
-      <keep-alive include="PostList,Chatroom,My,PostDetail">
+      <keep-alive include="PostList,Chatroom,My">
+        <!-- ,PostDetail  不缓存帖子详情页面了 -->
         <!-- 复用 PostDetail，且 key 使用 fullPath，可以缓存已浏览（且未活跃）的帖子，体验好一丢丢 -->
         <router-view :style="styleObj" :key="$route.fullPath" class="appView" />
       </keep-alive>
