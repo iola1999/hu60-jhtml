3476245c8dddbc2e84162ccf98d5f39b8bd51249
iola1999 <iola1999@foxmail.com>
2020-01-13 22:36:52 +0800

    返回帖子列表等页面时滚动到离开的位置

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -55,10 +55,10 @@ export default {
   mounted() {},
   // 在被缓存，且需要重新进入时返回离开时位置的页面，加上如下 activated、beforeRouteLeave
   activated() {
-    // setTimeout(() => {
-    //   const targetPosition = this.$route.meta.scrollTo || [0, 0];
-    //   window.scrollTo(...targetPosition);
-    // }, 300); // 250 毫秒的动画过渡时间，稍微再加点
+    setTimeout(() => {
+      const targetPosition = this.$route.meta.scrollTo || [0, 0];
+      window.scrollTo(...targetPosition);
+    }, 300); // 250 毫秒的动画过渡时间，稍微再加点
   },
   beforeRouteLeave(to, from, next) {
     from.meta.scrollTo = [window.scrollX, window.scrollY];
--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -47,16 +47,16 @@ export default {
   },
   mounted() {},
   // 在被缓存，且需要重新进入时返回离开时位置的页面，加上如下 activated、beforeRouteLeave
-  // activated() {
-  //   setTimeout(() => {
-  //     const targetPosition = this.$route.meta.scrollTo || [0, 0];
-  //     window.scrollTo(...targetPosition);
-  //   }, 300); // 250 毫秒的动画过渡时间，稍微再加点
-  // },
-  // beforeRouteLeave(to, from, next) {
-  //   from.meta.scrollTo = [window.scrollX, window.scrollY];
-  //   next();
-  // },
+  activated() {
+    setTimeout(() => {
+      const targetPosition = this.$route.meta.scrollTo || [0, 0];
+      window.scrollTo(...targetPosition);
+    }, 300); // 250 毫秒的动画过渡时间，稍微再加点
+  },
+  beforeRouteLeave(to, from, next) {
+    from.meta.scrollTo = [window.scrollX, window.scrollY];
+    next();
+  },
   methods: {
     loadPostList(pageNumber) {
       return hu60Api.listNewPosts(pageNumber).then((response) => {
