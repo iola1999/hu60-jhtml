3d221f321ddcbb47ffba44d7e23e2f7f9d69b7af
iola1999 <iola1999@foxmail.com>
2021-01-31 02:13:16 +0800

    聊天室初步demo

--- api/hu60Api.js
+++ api/hu60Api.js
@@ -13,3 +13,57 @@ export const listNewPosts = (pageNumber = 1) => {
 	};
 	return httpFetch.get(requestUrl, params);
 };
+
+/**
+ * 获取聊天室列表，其中第一项是最新发言聊天室
+ * @returns {Promise}
+ */
+export const getChatRoomList = () => {
+	const requestUrl = '/addin.chat.json';
+	return httpFetch.get(requestUrl, {});
+};
+
+/**
+ * 获取聊天室详细发言
+ * @param {String} name 聊天室名称
+ * @param {Number} page 第几页，默认是1
+ * @returns {Promise}
+ */
+export const getChatroomMsg = (name, page = 1) => {
+	const requestUrl = `/addin.chat.${name}.json?p=${page}`;
+	return httpFetch.get(requestUrl, {});
+};
+
+/**
+ * 获取帖子详情及回复
+ * @param {Number} topic_id 帖子id
+ * @param {Number} replyPageNumber 第几页，默认是1
+ * @returns {Promise}
+ */
+export const getPostDetailAndReply = (topic_id, replyPageNumber = 1) => {
+	const requestUrl = '/bbs.topic.' + topic_id + '.' + replyPageNumber + '.json';
+	return httpFetch.get(requestUrl, {});
+};
+
+/**
+ * 获取个人（本人）信息。设置回复楼层展示顺序也是这个接口
+ * @returns {Promise}
+ */
+export const getSelfInfo = (floorReverse) => {
+	let requestUrl = '';
+	if (floorReverse === '0' || floorReverse === '1') {
+		requestUrl = '/user.index.json?floorReverse=' + floorReverse;
+	} else {
+		requestUrl = '/user.index.json';
+	}
+	return httpFetch.get(requestUrl);
+};
+
+/**
+ * 登录
+ * @returns {Promise}
+ */
+export const login = (loginData) => {
+	const requestUrl = '/user.login.json';
+	return httpFetch.post(requestUrl, loginData);
+};
--- pages/chatroom/index.vue
+++ pages/chatroom/index.vue
@@ -1,24 +1,96 @@
 <template>
-	<view class="page-content">chatroom</view>
+	<view class="page-content">
+		<u-navbar :is-back="false" title="" :background="navbarBackground" :height="60">
+			<view class="chatroom-choose-wrap">
+				<u-icon name="chat" color="#ffffff" size="34"></u-icon>
+				<text class="chatroom-name-text">{{currentChatroomName}}</text>
+				<u-icon name="arrow-down-fill" color="#ffffff" size="24"></u-icon>
+			</view>
+		</u-navbar>
+		<view>
+			<view style="margin-bottom: 60px;">
+				<!-- 应该反序？向上滚动，再排查一下为什么滚动刷新无法触发。 -->
+				<view v-for="msgItem in chatMsgList" :key="msgItem.id">
+					<p> {{msgItem.uinfo.name}}</p>
+					<view style="width: 100%;">
+						<rich-text :nodes="msgItem.content"></rich-text>
+					</view>
+					<hr>
+				</view>
+			</view>
+			<view>
+				输入框
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {};
-	},
-	components: {},
-	onLoad() {},
-	onShow() {
-},
-onReady() {},
-	computed: {
-		myDevices() {
-			return this.$store.getters.myDevices;
+	import {
+		getChatRoomList,
+		getChatroomMsg
+	} from '@/api/hu60Api.js';
+	export default {
+		data() {
+			return {
+				navbarBackground: {
+					backgroundColor: '#53b1a8',
+					// 渐变色
+					// backgroundImage: 'linear-gradient(45deg, rgb(28, 187, 180), rgb(141, 198, 63))'
+				},
+				currentChatroomName: '',
+				chatroomList: [], // 全部聊天室列表，第一项（最新发言）是默认展示的。另外 没做新建聊天室（即手动输入名称进入聊天室）
+
+				loadedPageCount: 0, // 已加载的聊天室消息页数
+				maxPage: 1, // 一共多少页
+				totalMsgCount: 0,
+				chatMsgList: [],
+			};
 		},
-	},
-	methods: {}
-};
+		components: {},
+		onLoad() {
+			// created
+			this.getChatRoomList();
+		},
+		onReady() {
+			// mounted
+		},
+		computed: {},
+		methods: {
+			async getChatRoomList() {
+				// 聊天室列表，其中第一项是最新发言的，默认展示它
+				const roomList = (await getChatRoomList()).data.chatRomList
+				if (roomList.length > 0) {
+					this.currentChatroomName = roomList[0].name;
+					this.loadCurrentRoomMsg()
+				}
+			},
+			async loadCurrentRoomMsg() {
+				console.log('加载当前聊天室的消息')
+				const currentChatRoomInfo = (await getChatroomMsg(this.currentChatroomName, this.loadedPageCount)).data
+				this.loadedPageCount = currentChatRoomInfo.currPage
+				this.maxPage = currentChatRoomInfo.maxPage
+				this.totalMsgCount = currentChatRoomInfo.chatCount
+				this.chatMsgList.push(...currentChatRoomInfo.chatList);
+			},
+		},
+		watch: {}
+	};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+	.chatroom-choose-wrap {
+		display: flex;
+		align-items: center;
+		padding: 4px 6px;
+		background-color: rgba(240, 240, 240, 0.35);
+		color: #fff;
+		font-size: 18px;
+		border-radius: 100rpx;
+		margin-left: 30rpx;
+
+		.chatroom-name-text {
+			padding: 0 12rpx;
+		}
+	}
+</style>
